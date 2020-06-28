f4d6e7a005baeda387a38109d5b0ad52158a5c3d
xushancng <shancong.xu@newtouch.com>
2020-06-28 21:40:38 +0800

    枝干添加

--- src/main/java/com/atLearnSpace/git/HelloGit.java
+++ src/main/java/com/atLearnSpace/git/HelloGit.java
@@ -7,6 +7,9 @@ public class HelloGit {
 		System.out.println("更新1");
 		System.out.println("更新2");
 		System.out.println("分支添加");
+		
+		System.out.println("更新2");
+		System.out.println("分支添加");
 
 	}
 
